25ef1ba5e284bb9c392cb13b2f0b23be655a43e4
AlaynDev <saezalayn@gmail.com>
2023-04-24 00:40:28 -0300

    Authentication - QoS - Shared subscriptions - EMQX - Docker

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const mqtt = require("mqtt");
-const client = mqtt.connect("ws://test.mosquitto.org:8080/mqtt");
+const client = mqtt.connect("mqtt://localhost:1883/", {
+  username: "backend",
+  password: "mqtt-backend-password",
+});
 
 client.on("connect", function () {
   console.log("Connected to MQTT broker");
-  client.subscribe("application-iot-device-simulator", () => {
-    console.log("Subscribed to topic");
+  client.subscribe("$share/group1/value", { qos: 2 }, (err) => {
+    if (err) {
+      console.error("Error subscribing:", err);
+    } else {
+      console.log("Subscribed to topic");
+    }
   });
 });
 
@@ -17,8 +24,22 @@ client.on("message", function (topic, message) {
 
   if (messageValue > alertValue) {
     console.log("Send Alert email");
+
+    publishMessage("send-alert", "true");
+  } else {
+    publishMessage("send-alert", "false");
   }
 });
+
+function publishMessage(topic, message) {
+  client.publish(topic, message, { qos: 2 }, (err) => {
+    if (err) {
+      console.error("Error publishing message :", err);
+    } else {
+      console.log(`Message published successfully to topic ${topic}`);
+    }
+  });
+}
 
 client.on("reconnect", () => {
   console.error("Trying to reconnect");
--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,28 @@
-const client = mqtt.connect("ws://test.mosquitto.org:8080/mqtt");
+const client = mqtt.connect("ws://localhost:8083/mqtt", {
+  username: "frontend",
+  password: "mqtt-frontend-password",
+});
 
 client.on("connect", () => {
   console.log("Connected to MQTT broker");
+  client.subscribe("$share/group2/send-alert", { qos: 2 }, (err) => {
+    if (err) {
+      console.error("Error subscribing:", err);
+    } else {
+      console.log("Subscribed to topic");
+    }
+  });
 });
 
 function main() {
+  client.on("message", (topic, message) => {
+    console.log(topic, "-", JSON.parse(message));
+
+    if (JSON.parse(message))
+      document.querySelector(".show_alert").style.display = "block";
+    else document.querySelector(".show_alert").style.display = "none";
+  });
+
   const sendForm = document.querySelector(".form");
 
   sendForm.addEventListener("submit", (e) => {
@@ -20,8 +38,9 @@ function main() {
 
     if (client.connected) {
       client.publish(
-        "application-iot-device-simulator",
+        "value",
         JSON.stringify(data),
+        { qos: 2 },
         (err) => {
           if (err) {
             console.error("Error publishing message:", err);
